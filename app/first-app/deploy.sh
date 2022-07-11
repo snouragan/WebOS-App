063921f4514603dc3ee137b558e1da32852adecb
janelu44 <matei.galesanu@gmail.com>
2022-07-11 18:01:07 +0300

    add video player and sync

--- app/first-app/deploy.sh
+++ app/first-app/deploy.sh
@@ -2,14 +2,23 @@ cd ~/webOS_apps
 ares-package first-app/
 
 ares-install --device tv0 --remove com.aloalo.app.firstapp
+ares-install --device tv1 --remove com.aloalo.app.firstapp
+ares-install --device tv2 --remove com.aloalo.app.firstapp
+ares-install --device tv3 --remove com.aloalo.app.firstapp
 ares-install --device tv4 --remove com.aloalo.app.firstapp
 ares-install --device tv5 --remove com.aloalo.app.firstapp
 
 ares-install --device tv0 com.aloalo.app.firstapp_0.0.1_all.ipk
+ares-install --device tv1 com.aloalo.app.firstapp_0.0.1_all.ipk
+ares-install --device tv2 com.aloalo.app.firstapp_0.0.1_all.ipk
+ares-install --device tv3 com.aloalo.app.firstapp_0.0.1_all.ipk
 ares-install --device tv4 com.aloalo.app.firstapp_0.0.1_all.ipk
 ares-install --device tv5 com.aloalo.app.firstapp_0.0.1_all.ipk
 
 
 ares-launch --device tv0 com.aloalo.app.firstapp
+ares-launch --device tv1 com.aloalo.app.firstapp
+ares-launch --device tv2 com.aloalo.app.firstapp
+ares-launch --device tv3 com.aloalo.app.firstapp
 ares-launch --device tv4 com.aloalo.app.firstapp
 ares-launch --device tv5 com.aloalo.app.firstapp
