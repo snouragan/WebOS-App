599e5eec898908ce3367bd486d130ed3377ff396
snouragan <ahaiducescu@yahoo.com>
2022-07-19 01:39:23 -0700

    floating/clicked animations

--- client/tv.js
+++ client/tv.js
@@ -3,11 +3,15 @@ document.querySelectorAll('.TV').forEach((tv) => {
         console.log(tv.id);
         if (tv.classList.contains('clicked')) {
             tv.classList.remove('clicked');
+            tv.classList.add('floating');
         } else {
             tv.classList.add('clicked');
+            tv.classList.remove('floating');
             document.querySelectorAll('.TV').forEach((t) => {
                 if (t.id !== tv.id) {
                     t.classList.remove('clicked');
+                    t.classList.add('floating');
+                    
                 }
             })
         }
