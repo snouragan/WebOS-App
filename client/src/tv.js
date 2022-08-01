f32f0e1f6a0d572f4294376228fd281659fc434e
snouragan <ahaiducescu@yahoo.com>
2022-08-01 13:22:51 -0700

    gradient shadow

--- client/src/tv.js
+++ client/src/tv.js
@@ -1,6 +1,6 @@
     document.querySelectorAll('.TV').forEach((tv) => {
         tv.addEventListener('click', () => {
-            console.log(tv.id);
+            console.log(tv.id, "clicked");
             if (tv.classList.contains('clicked')) {
                 tv.classList.remove('clicked');
             } else {
@@ -14,3 +14,51 @@
             }
         });
     });
+
+    document.querySelectorAll('.TV').forEach((tv) => {
+        tv.addEventListener('mouseover', () => {
+            console.log(tv.id, "hover");
+            if(tv.id === "tv0") {
+                document.getElementById("mtv0").style.background = "red";
+            }
+            if(tv.id === "tv1") {
+                document.getElementById("mtv1").style.background = "red";
+            }
+            if(tv.id === "tv2") {
+                document.getElementById("mtv2").style.background = "red";
+            }
+            if(tv.id === "tv3") {
+                document.getElementById("mtv3").style.background = "red";
+            }
+            if(tv.id === "tv4") {
+                document.getElementById("mtv4").style.background = "red";
+            }
+            if(tv.id === "tv5") {
+                document.getElementById("mtv5").style.background = "red";
+            }
+        })
+    })
+
+    document.querySelectorAll('.TV').forEach((tv) => {
+        tv.addEventListener('mouseout', () => {
+            console.log(tv.id, "default");
+            if(tv.id === "tv0") {
+                document.getElementById("mtv0").style.background = "black";
+            }
+            if(tv.id === "tv1") {
+                document.getElementById("mtv1").style.background = "black";
+            }
+            if(tv.id === "tv2") {
+                document.getElementById("mtv2").style.background = "black";
+            }
+            if(tv.id === "tv3") {
+                document.getElementById("mtv3").style.background = "black";
+            }
+            if(tv.id === "tv4") {
+                document.getElementById("mtv4").style.background = "black";
+            }
+            if(tv.id === "tv5") {
+                document.getElementById("mtv5").style.background = "black";
+            }
+        })
+    })
