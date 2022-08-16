057bcc566c4e8912ead707299dbd174b1f47c461
snouragan <ahaiducescu@yahoo.com>
2022-08-16 17:49:19 +0300

    add file

--- client/src/file_upload.js
+++ client/src/file_upload.js
@@ -15,28 +15,156 @@ box.addEventListener('click', () => {
 
 });
 
+document.querySelector('input').addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+let selectTvF = 0;
+let selectTypeF = 0;
+let titleF = 0;
+let uploadF = 0;
+let formData = new FormData();
+
+document.getElementById('file-input').addEventListener('change', () => {
+
+    let options = [1, 2, 3, 4, 5, 6];
+    let selectTv = document.createElement('select');
+    selectTv.id = 'tvnum';
+
+    let type = ['stretch', 'fit'];
+    let selectType = document.createElement('select');
+    selectType.id = 'pictype';
+
+    let title = document.createElement('input');
+    title.id = 'title';
+    title.setAttribute('type', 'text');
+    title.setAttribute('value', 'title');
+
+    let upload = document.createElement('div');
+    upload.classList.add('upload');
+    upload.classList.add('card');
+    upload.id = 'upload';
+
+    file = document.getElementById('file-input').files[0];
+
+    if(formData != null)
+        formData.delete('file');
+        
+    formData.append('file', file);
+
+    console.log(file);
+
+    if (selectTvF == 0) {
+        document.querySelector('.addfile').appendChild(selectTv);
+
+        for (var i = 0; i < options.length; i++) {
+            var option = document.createElement("option");
+            option.value = options[i];
+            option.text = options[i];
+            option.id = 'option';
+            selectTv.appendChild(option);
+        }
+
+        selectTvF = 1;
+
+    }
+
+    if (selectTypeF == 0) {
+        document.querySelector('.addfile').appendChild(selectType);
+
+        for (var i = 0; i < type.length; i++) {
+            var types = document.createElement("option");
+            types.value = type[i];
+            types.text = type[i];
+            types.id = 'types';
+            selectType.appendChild(types);
+        }
+
+        selectTypeF = 1;
+
+    }
+
+    if(titleF == 0) {
+        document.querySelector('.addfile').appendChild(title);
+
+        titleF = 1;
+    }
+    
+    if(uploadF == 0) {
+        document.querySelector('.addfile').appendChild(upload);
+
+        uploadF = 1;
+    }
+
+}, false);
+
+let num = 0;
+let type = 'stretch';
+let title = 'title';
+
+document.querySelector('.addfile').addEventListener('click', (event) => {
+    if (event.target.id == 'tvnum') {
+        event.stopPropagation();
+    }
+    if (event.target.id == 'option') {
+        event.stopPropagation();
+    }
+    if (event.target.id == 'pictype') {
+        event.stopPropagation();
+    }
+    if (event.target.id == 'title') {
+        event.stopPropagation();
+    }
+    if (event.target.id == 'types') {
+        event.stopPropagation();
+    }
+    if (event.target.id == 'upload') {
+
+        formData.append('n', num);
+        formData.append('sf', type);
+        formData.append('title', title);
+
+        console.log(num);
+        console.log(type);
+        console.log(title);
+
+        fetch('http://192.168.1.109:8069/ctl/upload', 
+        {
+            method: 'POST',
+            body: formData
+        });
+        event.stopPropagation();
+    }
+});
+
+document.querySelector('.addfile').addEventListener('change', (event) => {
+    if (event.target.id == 'tvnum') {
+        num = event.target.value;
+    }
+    if (event.target.id == 'pictype') {
+        type = event.target.value;
+    }
+    if (event.target.id == 'title') {
+        title = event.target.value;
+    }
+});
+
 (function getList() {
     const filelist = document.querySelector('.filelist');
 
-    fetch('http://192.168.1.15:8069/ctl/list')
+    fetch('http://192.168.1.109:8069/ctl/list')
         .then(res => res.json())
         .then(data => {
-            list = data;
-            list.foreach(file => {
-                Object.entries(file).forEach(([key, value]) => {
-                    console.log('${key} ${value}');
+            
+            // for(let key in data) {
 
-                });
-                console.log('got json');
-            });
+            // }
+
+            console.log(data);
         });
 
     setTimeout(getList, 5000);
 })();
-
-box.addEventListener('transitionend', () => {
-    
-});
 
 // {
 //     "resources": {
